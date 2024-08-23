e1092e8f0e5a36f9198d1c9d85fa0dc0996a0632
marcosvcg <marcos.galvao@ucsal.edu.br>
2024-08-23 14:19:28 -0300

    FEAT: adiciona os exericicios do slide de heranca

--- README.md
+++
@@ -1,2 +0,0 @@
-# Resolução das listas de exercícios da Solutis Dev Trail 2024
-## Por: Marcos Vinícius da Costa Galvão
--- lista4/questao4/AnimalVoadorAB.java
+++
@@ -1,8 +0,0 @@
-package lista4.questao4;
-
-import lista4.questao2.AnimalAB;
-
-public abstract class AnimalVoadorAB extends AnimalAB {
-
-    public abstract void voar(int distancia);
-}
